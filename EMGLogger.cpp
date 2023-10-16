aac1d65560142a1beee6c36a85fac6baaf43e836
Leon Farchau <leon@example.com>
2023-10-16 23:57:10 +0200

    Implment MAX11254 in main

--- EMGLogger.cpp
+++ EMGLogger.cpp
@@ -11,6 +11,7 @@
 #include "hardware/adc.h"
 
 #include "comInterface.h"
+#include "max11254_hal.h"
 
 //include memcpy
 #include <string.h>
@@ -20,6 +21,14 @@
 #define DEBUG_PIN1 2
 #define DEBUG_PIN2 3
 
+#define ADC_CLK_PIN 10
+#define ADC_MOSI_PIN 11
+#define ADC_MISO_PIN 12
+#define ADC_CS_PIN 13
+#define ADC_RDYB_PIN 2
+#define ADC_RESET_PIN 5
+
+#define USE_ONBOARD_ADC 0
 
 // Data types
 typedef struct
@@ -29,7 +38,7 @@ typedef struct
 } Header;
 
 //variables
-uint16_t sampleRate = 50'000; //Hz
+uint16_t sampleRate = 2'000; //Hz
 uint16_t samplePeriod = 1'000'000/sampleRate; //us
 uint16_t gain = 1;
 Header header = {samplePeriod, gain};
@@ -42,14 +51,13 @@ uint32_t nextSampleTime = 0;
 void setup();
 void sendSampleBuffer(void* buffer, uint32_t length);
 
-int main()
-{
+int main(){
     setup();
    
 
     while(1)
     {
-
+        #if USE_ONBOARD_ADC
         volatile uint32_t currentTime = time_us_32();
         if(currentTime >= nextSampleTime)
         {
@@ -80,6 +88,18 @@ int main()
 
             gpio_put(DEBUG_PIN1, 0);
         }
+        #endif
+
+        // check if a conversion is finished
+        if(gpio_get(ADC_RDYB_PIN) == 0)
+        {
+            // read ADC and store in buffer
+            int32_t adcValue;
+            adcValue = (int32_t) max11254_hal_read_reg(MAX11254_DATA3_OFFSET);
+            adcValue = (adcValue & 0x00800000)? (adcValue | 0xFF000000) : adcValue;
+            //printf("ADC value: %d\n", adcValue);
+            comInterfaceAddSample(adcValue);
+        }
     }
 
     return 0;
@@ -109,5 +129,31 @@ void setup()
     adc_init();
     adc_gpio_init(26);
     adc_select_input(0);
+
+    // Init MAX11254
+    spi_inst_t *spiADC = spi1;
+    gpio_set_function(ADC_CLK_PIN, GPIO_FUNC_SPI);
+    gpio_set_function(ADC_MOSI_PIN, GPIO_FUNC_SPI);
+    gpio_set_function(ADC_MISO_PIN, GPIO_FUNC_SPI);
+    gpio_init(ADC_CS_PIN);
+    gpio_set_dir(ADC_CS_PIN, GPIO_OUT);
+    gpio_init(ADC_RDYB_PIN);
+    gpio_set_dir(ADC_RDYB_PIN, GPIO_IN);
+    gpio_init(ADC_RESET_PIN);
+    gpio_set_dir(ADC_RESET_PIN, GPIO_OUT);
+
+    gpio_put(ADC_CS_PIN, 1);
+    gpio_put(ADC_RESET_PIN, 1);
+
+    volatile uint32_t actualBaudRate = 0;
+    actualBaudRate = spi_init(spiADC, 8000000);
+    printf("Actual baud rate: %d\n", actualBaudRate);
+
+    // reset ADC
+    gpio_put(ADC_RESET_PIN, 0);
+    sleep_ms(1);
+    gpio_put(ADC_RESET_PIN, 1);
+
+    max11254_hal_init(spiADC, ADC_CS_PIN);
 }
     
