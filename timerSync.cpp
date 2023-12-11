4b9df3aec2d000ba50b037f846fee06c9253ae26
Leon Farchau <leon@example.com>
2023-12-11 11:58:10 +0100

    Improve accuracy of the synchronization

--- timerSync.cpp
+++ timerSync.cpp
@@ -11,7 +11,7 @@
 
 // Defines
 #define ALPHA 0.1 // The alpha value for the low pass filter
-#define DEVIATION_FOR_SYNC 2 // The number of clock cycles that the timer is allowed to deviate from the expected value for the sync flag is set
+#define DEVIATION_FOR_SYNC 15 // The number of clock cycles that the timer is allowed to deviate from the expected value for the sync flag is set
 
 // Global Variables
 volatile uint32_t g_cyclesPerSync = 0; // Number of derived clock cycles per sync pulse
@@ -61,18 +61,21 @@ void initSync(uint32_t inputFreq, uint32_t timesPerSync, uint32_t pin, uint32_t 
     // to the sync input will be done while running
     pwm_config pwmConfig =  pwm_get_default_config();
 
-    // Set the prescaler so that the timer runs at 1MHz
+    // Set the prescaler as low as possible to get the highest possible resolution
+    //  But to account for errors in the clock speed a frequency of 0.5 * outFreq should be possible
+    uint32_t outFreq = inputFreq * timesPerSync;
     uint32_t clockSpeed = clock_get_hz(clk_sys);
-    uint32_t prescaler = clockSpeed / 1000000;
+    float prescaler = clockSpeed / ((float)UINT16_MAX * (float)outFreq * 0.5f);
     pwm_config_set_clkdiv(&pwmConfig, prescaler);
 
     // Set the period of the timer to be 1/outputFreq seconds
-    g_timerPeriod = 1'000'000.0 / ((float)inputFreq * (float)timesPerSync);
+    g_timerPeriod = ((float)clockSpeed / prescaler) / ((float)outFreq );
     pwm_config_set_wrap(&pwmConfig, g_timerPeriod);
 
     pwm_clear_irq(g_timer_slice);
     pwm_set_irq_enabled(g_timer_slice, true);
     irq_set_exclusive_handler(PWM_IRQ_WRAP, timerSyncIRQ);
+    irq_set_priority(PWM_IRQ_WRAP, PICO_HIGHEST_IRQ_PRIORITY);
     irq_set_enabled(PWM_IRQ_WRAP, true);
 
     // Init and start the timer
@@ -98,23 +101,31 @@ void syncIRQ(uint pin, uint32_t eventMask)
     uint32_t counterVal = *g_pwm_ctr;
     uint32_t cyclesSinceSync = g_cyclesSinceSync;
 
-    // Reset the timer
-    *g_pwm_ctr = 0;
+    // adjust the phase of the timer
+    *g_pwm_ctr = cyclesSinceSync < g_cyclesPerSync? g_timerPeriod-1 : 0;
 
-    // Call the sync callback
-    if(g_syncCallback != NULL)
+
+
+    // add interrupt latency ~2µs
+    counterVal += 150; // 128 clock cycles is roughly 2µs
+    if (counterVal < 0)
     {
-        g_syncCallback(g_isSync);
+        counterVal = 0;
     }
+    
 
     // Calculate the new period of the timer
     int32_t totalClockCycles = counterVal + cyclesSinceSync * g_timerPeriod;
     int32_t clocksError = (totalClockCycles - (int32_t)g_cyclesPerSync * (int32_t)g_timerPeriod)/(int32_t)g_cyclesPerSync;
     g_isSync |= abs(clocksError) < DEVIATION_FOR_SYNC; 
 
-    uint32_t newPeriod = (int32_t) g_timerPeriod - (clocksError * ALPHA);
-    
-    pwm_set_wrap(g_timer_slice, newPeriod);
+    uint32_t newPeriod = (int32_t) g_timerPeriod + (clocksError * ALPHA);
+    g_timerPeriod = newPeriod;
+    if(g_timerPeriod > UINT16_MAX)
+    {
+        g_timerPeriod = UINT16_MAX;
+    }
+    pwm_set_wrap(g_timer_slice, g_timerPeriod);
 
     // Reset the cycles since sync counter
     g_cyclesSinceSync = 0;
